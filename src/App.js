d2f596589eb65906f63b16d90ed935654a958405
Lestergil7 <lestergildiaz@gmail.com>
2022-08-27 17:40:48 -0300

    2da entrga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import navBar from "./components/navBar";
+function App(props) {
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='bg-orange-200 h-screen'>
+      <div className="bg-white h-36">
+     <navBar></navBar>
+      </div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
