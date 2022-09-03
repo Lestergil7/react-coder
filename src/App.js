5f9b5eec722d78b72bf0a8b3772eb88aa59fd6e0
Lestergil7 <lestergildiaz@gmail.com>
2022-09-03 20:32:53 -0300

    entrega

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
-import {useState} from 'react';
 import ItemCount from "./components/ItemCount";
 
 
-function App() {
+function App(props) {
 
-  const [counter,SetCounter] = useState(0)
-    
-  const handleClick =()=>{
-      SetCounter(counter + 1);
-      console.log('Hiciste click')
-  }
+  
 
   return (
     
@@ -19,12 +13,7 @@ function App() {
      <div className="bg-neutral-50 h-full   mx-9 my-9 ">
       <NavBar></NavBar>
       <Header></Header>
-
-      <div className='flex'>
-        <strong>Contador: {counter}</strong>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' onClick={handleClick}> Boton</button>
-    </div>
-
+       <ItemCount/>
      </div>
     </div>
   )
